dee0b5a167732cf481e982b5d9a50bf5bfa5ae7f
O`lmasbek <oolmasbek754@gmail.com>
2026-04-07 15:18:39 +0500

    pal gemes

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ let score = 0;
 function addScore() {
   score++;
   document.getElementById("score").innerText = score;
+
+  if (score === 500) {
+    document.getElementById("winImage").style.display = "block";
+  }
 }
